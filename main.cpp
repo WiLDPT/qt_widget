11fa5bbe0a5203d78b14e4a60069881ec22bedd6
Pedro Fernandes <WiLDPT@users.noreply.github.com>
2020-05-08 13:24:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,21 @@
 
 #include <QApplication>
 #include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
 
-    QLabel *label = new QLabel("Gametime!");
-    label->show();
+    //QLabel *label = new QLabel("Gametime!");
+    //label->show();
+
+    //QPushButton *button = new QPushButton("Quit the program");
+    //QObject::connect(button, SIGNAL(clicked()), &a, SLOT(quit()));
+    //button->show();
+
 
     return a.exec();
 }
