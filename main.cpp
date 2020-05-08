ea2fec897a6dd80b6ee96fdb043ec5452815cce0
Pedro Fernandes <WiLDPT@users.noreply.github.com>
2020-05-08 13:10:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+
+    QLabel *label = new QLabel("Gametime!");
+    label->show();
+
     return a.exec();
 }
